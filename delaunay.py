2e9bc64ed3b8f171232ea952ff0e7ef244bc11a5
Oscar Fontaine <oscar.fontaine@free.fr>
2024-05-23 11:10:24 +0200

    Corrected name problem in FlatTriangulation

--- delaunay.py
+++ delaunay.py
@@ -27,13 +27,19 @@ class FlatTriangulation:
             if (a,b) not in labelling:
                 (c,d) = M.opposite_edge(a,b)
                 labelling[a,b]=len(labelling)
-                self.vectors.append(S.tangent_vector(a,S.polygon(a).vertex(b),S.polygon(a).edge(b)))
+                self.vectors.append(M.tangent_vector(a,M.polygon(a).vertex(b),M.polygon(a).edge(b)))
                 labelling[c,d]=len(labelling)
-                self.vectors.append(S.tangent_vector(c,S.polygon(c).vertex(d),S.polygon(c).edge(d)))
+                self.vectors.append(M.tangent_vector(c,M.polygon(c).vertex(d),M.polygon(c).edge(d)))
         for lab in M.labels():
             for i in range(3):
                 fp[labelling[lab, i]]=labelling[lab, (i+1)%3]
         self.triangulation = FatGraph(ep=ep, fp=fp) # the graph given by the triangulation
+
+    def copy(self):
+        nt = FlatTriangulation(self.surface)
+        nt.vectors = self.vectors[:]
+        nt.triangulation = self.triangulation.copy()
+        return nt
 
     def is_flipable(self,e1):
         if e1%2 == 0:
@@ -96,8 +102,30 @@ class FlatTriangulation:
         pol2 = lab_pol
         ver2 = lab_ver
 
-        self.vectors[e1] = S.tangent_vector(pol1, S.polygon(pol1).vertex(ver1), hol1)
-        self.vectors[e2] = S.tangent_vector(pol2, S.polygon(pol2).vertex(ver2), hol2)
+        self.vectors[e1] = M.tangent_vector(pol1, M.polygon(pol1).vertex(ver1), hol1)
+        self.vectors[e2] = M.tangent_vector(pol2, M.polygon(pol2).vertex(ver2), hol2)
+
+    
+    def geodesic(self, e1): #return the hyperbolic geometric associate to the edge with label e1 in the corresponding triangulation
+        if e1%2 == 0:
+            e2 = e1+1
+        else : e2 = e1-1
+        fp = self.triangulation.face_permutation(copy = False)
+        f1 = fp[e1]
+        f2 = fp[f1]
+        g1 = fp[e2]
+        g2 = fp[g1]
+        x1 = t.vectors[g1].vector()[0]
+        y1 = t.vectors[g1].vector()[1]
+        x2 = t.vectors[e1].vector()[0]
+        y2 = t.vectors[e1].vector()[1]
+        V = -t.vectors[f2].vector()
+        x3 = V[0]
+        y3 = V[1]
+        a = x1*y2*y3*(y3-y2) + x2*y1*y3*(y1-y3) + x3*y1*y2*(y2-y1)
+        b = x1*y1*(x2*y3-x3*y2) + x2*y2*(x3*y1-x1*y3) + x3*y3*(x1*y2-x2*y1)
+        c = x1*x2*y3*(x1-x2) + x2*x3*y1*(x2-x3) + x1*x3*y2*(x3-x1)
+        return H.geodesic(a,2*b,c=c,model = "half_plane")
     
     def plot(self, *args, **kwds):
         res = Graphics()
@@ -107,3 +135,40 @@ class FlatTriangulation:
             res = res + traj.plot(*args, **kwds)
         return res
 
+
+
+
+
+class IsoDelaunayCell:
+    def __init__(self,t):
+        self.triangulation = t.copy()
+        Passage1 = True
+        n = len(t.vectors)
+        fp = t.triangulation.face_permutation(copy = False)
+        cor = {}
+        for i in range(0,n,2):
+            g = t.geodesic(i)
+            if g in cor:
+                cor[g].append(i)
+            else:
+                cor[g] = [i]
+            if Passage1:
+                Sol = g.left_half_space()
+                Passage1 = False
+            else:
+                Sol = H.intersection(Sol, g.left_half_space())
+        self.polygon = Sol
+        e = [s.geodesic() for s in Sol.edges()]
+        cor_final = {}
+        for g in e:
+            cor_final[g] = cor[g][:]
+        self.correspondance = cor_final
+
+    def plot(self, *args, **kwds):
+        return self.polygon.plot(*args, **kwds)
+
+
+
+
+
+        
