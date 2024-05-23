5d3c2bf428649c6d7f1f19471daf27d06597e455
Vincent Delecroix <vincent.delecroix@u-bordeaux.fr>
2024-05-23 11:55:57 +0200

    fix eddge flip + isomorphism test function

--- delaunay.py
+++ delaunay.py
@@ -33,13 +33,116 @@ class FlatTriangulation:
         for lab in M.labels():
             for i in range(3):
                 fp[labelling[lab, i]]=labelling[lab, (i+1)%3]
-        self.triangulation = FatGraph(ep=ep, fp=fp) # the graph given by the triangulation
+
+        # NOTE: on recent versions of surface_dynamics, ep is forced to be
+        # 0 <-> 1, 2 <-> 3, etc and is not anymore an argument of the constructor
+        try:
+            self.triangulation = FatGraph(ep=ep, fp=fp) # the graph given by the triangulation
+        except TypeError:
+            self.triangulation = FatGraph(fp=fp) # the graph given by the triangulation
+
+    def _check(self):
+        self.triangulation._check()
+
+        fp = self.triangulation.face_permutation(copy=False)
+        n = len(self.vectors)
+        for e1 in range(0, n, 2*n):
+            v1 = self.vectors[e1].vector()
+            if v1 != -self.vectors[e1 + 1].vector():
+                raise ValueError('edge ({}, {}) have vectors with non-opposite holonomies'.format(e1, e1+1))
+
+            e2 = fp[e1]
+            v2 = self.vectors[e2].vector()
+            e3 = fp[e2]
+            v3 = self.vectors[e3].vector()
+            if not (v1 + v2 + v3).is_zero() or ccw(v1, v2) <= 0 or ccw(v2, v3) <= 0 or ccw(v3, v1) <= 0:
+                raise ValueError('({}, {}, {}) is not a valid triangle'.format(e1, e2, e3))
+
+    def __repr__(self):
+        return 'Flat Triangulation of a {}'.format(self.surface)
 
     def copy(self):
         nt = FlatTriangulation(self.surface)
         nt.vectors = self.vectors[:]
         nt.triangulation = self.triangulation.copy()
         return nt
+
+    def is_isomorphic(self, other, certificate=False):
+        r"""
+        Test whether ``self`` and ``other`` defines the same triangulation of the underlying surface.
+
+        OUTPUT: if ``certificate=False`` then the output is a boolean. If ``certificate=True`` then
+        the output is a pair ``(answer, relabelling)`` where ``answer`` is a boolean and ``relabelling``
+        is ``None`` if ``answer=False`` and is a permutation of labels if ``answer=True``.
+
+        EXAMPLES::
+
+            sage: from flatsurf import translation_surfaces
+            sage: s = translation_surfaces.arnoux_yoccoz(3)
+            sage: t0 = FlatTriangulation(s)
+            sage: t = t0.copy()
+            sage: t0.is_isomorphic(t)
+            True
+            sage: t0.is_isomorphic(t, certificate=True)
+            (True, [0, 1, 2, 3, ..., 35])
+
+        Now flip an edge twice so that the labels get flipped::
+
+            sage: e = choice([e for e in range(2 * t.triangulation.num_edges()) if t.is_flipable(e)])
+            sage: t.flip(e)
+            sage: t0.is_isomorphic(t)
+            False
+            sage: t.flip(e)
+            sage: t0.is_isomorphic(t)
+            True
+            sage: ans, certificate = t0.is_isomorphic(t, certificate=True)
+            sage: ans
+            True
+            sage: e != certificate[e]
+            True
+        """
+        if not isinstance(other, FlatTriangulation):
+            raise TypeError('can not compare t of type {} with a FlatTriangulation'.format(type(t).__name__))
+        if self.surface is not t.surface:
+            raise ValueError('can not compare FlatTriangulation on different underlying surfaces')
+
+        # find the first vector of self in other
+        n = len(self.vectors)
+        u0 = self.vectors[0]
+        i = 0
+        while i < n:
+            v = other.vectors[i]
+            if u0 == v:
+                break
+            i += 1
+        if i == n:
+            return (False, None) if certificate else False
+
+        # does simultaneous DFS
+        sfp = self.triangulation.face_permutation(copy=False)
+        ofp = other.triangulation.face_permutation(copy=False)
+        relabelling = [None] * n
+        relabelling[0] = i
+        relabelling[1] = i ^ 1
+        todo = [0, 1]
+        while todo:
+            i = todo.pop()
+            ii = relabelling[i]
+            assert ii is not None
+
+            for _ in range(3):
+                if relabelling[i] is None:
+                    if self.vectors[i] != other.vectors[ii]:
+                        return (False, None) if certificate else False
+                    relabelling[i] = ii
+                    relabelling[i ^ 1] = ii ^ 1
+                    todo.append(i ^ 1)
+                elif relabelling[i] != ii:
+                    return (False, None) if certificate else False
+                i = sfp[i]
+                ii = ofp[ii]
+
+        return (True, relabelling) if certificate else True
 
     def is_flipable(self,e1):
         if e1%2 == 0:
@@ -60,7 +163,7 @@ class FlatTriangulation:
         if e1%2 == 0:
             e2 = e1+1
         else : e2 = e1-1
-        fp = self.triangulation.face_permutation(copy = False)
+        fp = self.triangulation.face_permutation(copy=True)
         f1 = fp[e1]
         f2 = fp[f1]
         g1 = fp[e2]
@@ -71,6 +174,14 @@ class FlatTriangulation:
         fp[e2] = g2
         fp[g2] = f1
         fp[f1] = e2
+        # TODO: in order to not mess up the internal structure of FatGraph we build a new
+        # FatGraph... which is not optimal
+        ep = [2*(i//2)+(1- i%2) for i in range(len(M.edges()))]
+        try:
+            self.triangulation = FatGraph(ep=ep, fp=fp)
+        except TypeError:
+            self.triangulation = FatGraph(fp=fp)
+
         # modification of the vectors
         hol2 = self.vectors[g1].vector()+self.vectors[f2].vector()
         hol1 = -hol2
